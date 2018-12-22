e72334a8b5734f2611b73b7e2edfc771b69cf094
Umesh <umesh.khunt@gmail.com>
2018-12-21 22:05:22 -0800

    Change in app.js

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express();
+const port = process.env.PORT || 8080;
+const validUrl = require('valid-url');
 
-app.use(async (req, res) => {
-    const url = req.query.url;
-
-    if (!url) {
-        return res.send('Please provide URL as GET parameter, for example: <a href="/?url=https://example.com">?url=https://example.com</a>');
+var parseUrl = function(url) {
+    url = decodeURIComponent(url)
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = 'http://' + url;
     }
 
-    const browser = await puppeteer.launch({
-        args: ['--no-sandbox']
-    });
-    const page = await browser.newPage();
-    await page.goto(url);
-    const imageBuffer = await page.screenshot();
-    browser.close();
+    return url;
+};
 
-    res.set('Content-Type', 'image/png');
-    res.send(imageBuffer);
+app.get('/', function(req, res) {
+    var urlToScreenshot = parseUrl(req.query.url);
+
+    if (validUrl.isWebUri(urlToScreenshot)) {
+        console.log('Screenshotting: ' + urlToScreenshot);
+        (async() => {
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
+
+            const page = await browser.newPage();
+            await page.goto(urlToScreenshot);
+            await page.screenshot().then(function(buffer) {
+                res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
+                res.setHeader('Content-Type', 'image/png');
+                res.send(buffer)
+            });
+
+            await browser.close();
+        })();
+    } else {
+        res.send('Invalid url: ' + urlToScreenshot);
+    }
+
 });
 
 const server = app.listen(process.env.PORT || 8080, err => {
